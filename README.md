cbdaf3cbbfac9ec4fdd98d7097e55e0c12168ce4
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2013-05-01 12:44:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::Middleware::BetterStackTrace - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Middleware::BetterStackTrace;
+
+# DESCRIPTION
+
+Plack::Middleware::BetterStackTrace is ...
+
+# LICENSE
+
+Copyright (C) Tasuku SUENAGA a.k.a. gunyarakun.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
---
+++ lib/Plack/Middleware/BetterStackTrace.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::BetterStackTrace;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::BetterStackTrace - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::BetterStackTrace;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::BetterStackTrace is ...
+
+=head1 LICENSE
+
+Copyright (C) Tasuku SUENAGA a.k.a. gunyarakun.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tasuku SUENAGA a.k.a. gunyarakun E<lt>tasuku-s-github@titech.acE<gt>
+
+=cut
+
